29da0779fc3949ec5ce6bce6ec213445cf898cc9
Mike Shippy <mikeship04@gmail.com>
2022-07-29 09:07:57 -0700

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+ const [test, setTest] = useState('hello')
+
+  useEffect(() => {
+    fetch("http://localhost:9292/test")
+    .then(res => res.json())
+    .then((data) => setTest(data.message))
+  },[])
+
   return (
     <div className="App">
-    Hey!
+    Hey! {test}
     </div>
   );
 }
