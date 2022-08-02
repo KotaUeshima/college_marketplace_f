d6e3994a72b4f574f182850a2653540747024a17
Kota Ueshima <ueshima@usc.edu>
2022-08-01 19:47:21 -0700

    sign up form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CollegePage from './views/CollegePage';
 import PostPage from './views/PostPage';
 import NavBar from './views/NavBar';
 import UserPage from './views/UserPage';
+import SignUpPage from './views/SignUpPage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -33,6 +34,7 @@ function App() {
           <Route path="/:college_name" element={<PostPage login={login} loggedIn={loggedIn}/>} />
           <Route path="/login" element={<LoginPage handleLoginState={handleLoginState}/>} />
           <Route path="/my_posts" element={<UserPage login={login}/>} />
+          <Route path="/signup" element={<SignUpPage />} />
         </Routes>
       </BrowserRouter>
   );
