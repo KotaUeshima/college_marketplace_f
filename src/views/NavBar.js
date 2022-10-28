97655154c0a3862d78dd5afd904c109317e72d47
Kota Ueshima <ueshima@usc.edu>
2022-10-27 22:57:55 -0700

    test

--- src/views/NavBar.js
+++ src/views/NavBar.js
@@ -1,84 +1,79 @@
-import React from 'react'
-import {Link, useLocation} from 'react-router-dom'
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
-import Chip from '@mui/material/Chip';
-import Divider from '@mui/material/Divider';
-import HomeIcon from '@mui/icons-material/Home';
-import SchoolIcon from '@mui/icons-material/School';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { loggedIn, userState } from './atoms';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Chip from "@mui/material/Chip";
+import Divider from "@mui/material/Divider";
+import HomeIcon from "@mui/icons-material/Home";
+import SchoolIcon from "@mui/icons-material/School";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { loggedIn, userState } from "./atoms";
 
 function NavBar() {
+  const [recoilLogin, setRecoilLogin] = useRecoilState(loggedIn);
+  const user = useRecoilValue(userState);
 
-    const [recoilLogin, setRecoilLogin] = useRecoilState(loggedIn)
-    const user = useRecoilValue(userState)
-
-    function handleLogOut(){
-        if(recoilLogin){
-            setRecoilLogin(false)
-        }
+  function handleLogOut() {
+    if (recoilLogin) {
+      setRecoilLogin(false);
     }
+  }
 
-    const appStyle = {background: 'white',boxShadow: 'none'}
-    const linkStyle = {textDecoration: 'none', color: '#3d110e'}
-    const chipStyle = {backgroundColor:'#e48b27', color:'#3d110e'}
-    const welcomeStyle = {color:'#3d110e'}
-    const location = useLocation()
+  const appStyle = { background: "white", boxShadow: "none" };
+  const linkStyle = { textDecoration: "none", color: "#3d110e" };
+  const chipStyle = { backgroundColor: "#e48b27", color: "#3d110e" };
+  const welcomeStyle = { color: "#3d110f" };
+  const location = useLocation();
 
   return (
     <>
-        {(location.pathname === "/")? null :
-        <Box sx={{flexGrow: 1}}>
-            <AppBar 
-            style={appStyle} 
-            elevation={0} 
-            position="sticky">
-                <Toolbar>
-                    <Link to="/colleges" style={{ textDecoration: 'none' }}>
-                        <Chip
-                            color='primary'
-                            style={chipStyle}
-                            icon ={<SchoolIcon/>} 
-                            label="College Marketplace"
-                        />
-                    </Link>
-                    <Typography
-                        sx={{ flexGrow: 1, ml: 4}}
-                        style={welcomeStyle}
-                        >
-                        {recoilLogin? `Welcome Back ${user.username}!` : null}
-                    </Typography>
-                    <Link to="/colleges" style={linkStyle}>
-                        <IconButton
-                            size="large"
-                            edge="start"
-                            color="inherit"
-                            aria-label="menu"
-                            sx={{ mr: 2 }}
-                        >
-                            <HomeIcon/>   
-                        </IconButton>
-                        </Link> 
-                        {recoilLogin ? 
-                        <Link to="/my_posts" style={linkStyle}>
-                            <Button color="inherit">My posts</Button>
-                        </Link> : null}
-                        <Link to={recoilLogin? "/colleges" : "/login"} style={linkStyle}>
-                        <Button onClick={handleLogOut} color="inherit">
-                        {recoilLogin? "Logout" : "Login"}
-                        </Button>
-                    </Link>
-                </Toolbar>
-            </AppBar>
-            <Divider />
-        </Box>}
+      {location.pathname === "/" ? null : (
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar style={appStyle} elevation={0} position="sticky">
+            <Toolbar>
+              <Link to="/colleges" style={{ textDecoration: "none" }}>
+                <Chip
+                  color="primary"
+                  style={chipStyle}
+                  icon={<SchoolIcon />}
+                  label="College Marketplace"
+                />
+              </Link>
+              <Typography sx={{ flexGrow: 1, ml: 4 }} style={welcomeStyle}>
+                {recoilLogin ? `Welcome Back ${user.username}!` : null}
+              </Typography>
+              <Link to="/colleges" style={linkStyle}>
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  sx={{ mr: 2 }}
+                >
+                  <HomeIcon />
+                </IconButton>
+              </Link>
+              {recoilLogin ? (
+                <Link to="/my_posts" style={linkStyle}>
+                  <Button color="inherit">My posts</Button>
+                </Link>
+              ) : null}
+              <Link to={recoilLogin ? "/colleges" : "/login"} style={linkStyle}>
+                <Button onClick={handleLogOut} color="inherit">
+                  {recoilLogin ? "Logout" : "Login"}
+                </Button>
+              </Link>
+            </Toolbar>
+          </AppBar>
+          <Divider />
+        </Box>
+      )}
     </>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
